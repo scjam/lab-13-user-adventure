adc5272b139bcd23760ba8e2c1cdf47c479fa931
Sydney James <sydneyjames7@gmail.com>
2020-10-15 15:21:15 -0700

    first function written, begining data entry in data.js

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 // import functions and grab DOM elements
+const form = document.querySelector('form');
 
 // initialize state
 
 // set event listeners to update state and DOM
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const data = new FormData(form);
+
+    console.log(data.get('name'), data.get('class'));
+});
---
+++ data.js
@@ -0,0 +1,57 @@
+export default [
+    {
+        id: 'peppermint',
+        title: 'Peppermint Stick Forest',
+        map: {
+            top: '95%'
+            left: '95%'
+        },
+        image: '',
+        description: ``,
+        choices: [{
+            id: 'negotiate',
+            description: '',
+            result: ``,
+            candy:
+            friendship:
+        }, {
+            id: 'fight',
+            description: '',
+            result: ``,
+            candy:
+            friendship:
+        }, {
+            id: 'run',
+            description: '',
+            result: ``,
+            candy:
+            friendship:
+        }]
+    },
+    {
+        id: '',
+        title: '',
+        map: {},
+        image: '',
+        description: ``,
+        choices: [{
+            id: 'negotiate',
+            description: '',
+            result: ``,
+            candy:
+            friendship:
+        }, {
+            id: 'fight',
+            description: '',
+            result: ``,
+            candy:
+            friendship:
+        }, {
+            id: 'run',
+            description: '',
+            result: ``,
+            candy:
+            friendship:
+        }]
+    },
+];
